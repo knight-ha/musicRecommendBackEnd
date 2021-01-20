6a0928bf3a6296437a4fb9b1b63ff1dce74963be
lisy <1160283140@qq.com>
2021-01-20 09:42:10 +0800

    test

---
+++ src/main/java/com/knightha/makoreto/MakoretoApplication.java
@@ -0,0 +1,13 @@
+package com.knightha.makoreto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakoretoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakoretoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knightha/makoreto/MakoretoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knightha.makoreto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakoretoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
